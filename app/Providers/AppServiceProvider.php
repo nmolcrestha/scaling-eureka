24186c1d735815f70a398c45b820759a0f184b6e
Anmol <nmol.crestha@gmail.com>
2026-03-05 12:58:33 +0545

    Added policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use App\Policies\OrderPolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\UserPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Product::class, ProductPolicy::class);
+        Gate::policy(Order::class, OrderPolicy::class);
+        Gate::policy(User::class, UserPolicy::class);
+        Gate::before(function ($user, $ability) {
+            if ($user->has('super-admin')) {
+                return true;
+            }
+        });
     }
 }
